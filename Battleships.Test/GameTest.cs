8f93b44cf5f47fd3c18ba6161bf0fbff41e7d3a1
ddwived3 <40729727+ddwived3@users.noreply.github.com>
2022-08-11 15:56:12 -0400

    Instructions implemented

--- Battleships.Test/GameTest.cs
+++ Battleships.Test/GameTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Battleships;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace Battleships.Test
@@ -15,6 +10,30 @@ namespace Battleships.Test
         {
             var ships = new[] { "3:2,3:5" };
             var guesses = new[] { "7:0", "3:3" };
+            Game.Play(ships, guesses).Should().Be(0);
+        }
+
+        [Fact]
+        public void TestPlay_HitAll_Sunk4CellShip()
+        {
+            var ships = new[] { "3:1,6:1" };
+            var guesses = new[] { "3:1", "4:1", "5:1", "6:1" };
+            Game.Play(ships, guesses).Should().Be(1);
+        }
+
+        [Fact]
+        public void TestPlay_HitAll_Sunk2CellShips()
+        {
+            var ships = new[] { "1:1,2:1", "3:2,3:3" };
+            var guesses = new[] { "1:1", "2:1", "3:2", "3:3" };
+            Game.Play(ships, guesses).Should().Be(2);
+        }
+
+        [Fact]
+        public void TestPlay_HitAll_SinkShipCount()
+        {
+            var ships = new[] { "3:2,3:5" };
+            var guesses = new[] { "3:2", "3:3", "3:4" };
             Game.Play(ships, guesses).Should().Be(0);
         }
     }
--- Battleships/Game.cs
+++ Battleships/Game.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Battleships
 {
@@ -17,10 +14,170 @@ namespace Battleships
         //   e.g. "3:2,3:5" is a 4 cell ship horizontally across the 4th row from the 3rd to the 6th column
         // guesses: each string represents the co-ordinate of a guess
         //   e.g. "7:0" - misses the ship above, "3:3" hits it.
-        // returns: the number of ships sunk by the set of guesses
+        // returns: the number of ships sunk by the set of guesses       
         public static int Play(string[] ships, string[] guesses)
-        {
-            return 0;
+        {            
+            //sunk ships counter
+            int sunkShipsCounter = 0;
+
+            //create board of 10x10
+            char[,] board = CreateBoard();
+
+            //Place the ships on the board
+            board = PlaceShips(ships, board, out var numberOfShips);
+
+            // guess and fire (Hit=>H or Miss=>M)
+            foreach (var guess in guesses)
+            {
+                //Fire: hit or miss, update the board.
+                board = Fire(guess, board);
+            }
+
+            // Read hit position from the board,
+            // and place it in temp dictionary
+            var temp = new Dictionary<string, char>();
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == 'H')
+                        temp[$"{i},{j}"] = board[i, j];
+                }
+            }            
+
+            //Check where all cells (representing ship) are hit
+            //or not, if hit, increment numberOfSunkShips counter.
+            foreach (var key in numberOfShips.Keys)
+            {
+                if (temp.ContainsKey(key))
+                {
+                    int hitCount = 0;
+                    foreach (var item in numberOfShips[key])
+                    {
+                        if (temp.ContainsKey(item.Key))
+                        {
+                            hitCount++;
+
+                            //remove the position from temp to
+                            //ignore the repeatation.
+                            temp.Remove(item.Key);
+                        }
+                    }
+
+                    //hit counts == ship position counts => ship sunk
+                    if (hitCount == numberOfShips[key].Count)
+                        sunkShipsCounter++;
+                }
+            }
+
+            //number of sunk ships
+            return sunkShipsCounter;
         }
+
+        /// <summary>
+        /// Fire if hit set to H else M
+        /// </summary>
+        /// <param name="guess"></param>
+        /// <param name="board"></param>
+        /// <returns></returns>
+        private static char[,] Fire(string guess, char[,] board)
+        {
+            var guessPosition = GetPositions(guess.Split(':'));
+
+            //S represent the marker for ship.
+            board[guessPosition.X, guessPosition.Y] = board[guessPosition.X, guessPosition.Y] == 'S' ? 'H' : 'M';
+
+            return board;
+        }
+
+        /// <summary>
+        /// Place ship on the board.
+        /// </summary>
+        /// <param name="ships"></param>
+        /// <param name="board"></param>
+        /// <param name="numberOfShips"></param>
+        /// <returns></returns>
+        private static char[,] PlaceShips(string[] ships, char[,] board, out Dictionary<string, Dictionary<string, char>> numberOfShips)
+        {
+            numberOfShips = new Dictionary<string, Dictionary<string, char>>();
+            foreach (var ship in ships)
+            {
+                string[] arr = ship.Split(',');
+                var firstPosition = GetPositions(arr[0].Split(':'));
+                var lastPosition = GetPositions(arr[1].Split(':'));
+
+                Dictionary<string, char> shipDictionary = new Dictionary<string, char>();
+                string shipStartPosition = null;
+                if (firstPosition.X == lastPosition.X)
+                {
+                    //horizontal => x direction => column
+                    for (int i = firstPosition.Y; i <= lastPosition.Y; i++)
+                    {
+                        board[firstPosition.X, i] = 'S';
+                        shipDictionary[firstPosition.X + "," + i] = board[firstPosition.X, i];
+                        if(string.IsNullOrEmpty(shipStartPosition))
+                            shipStartPosition = $"{firstPosition.X},{i}";
+                    }
+                }
+                else
+                {
+                    //vertical => y direction => row
+                    for (int i = firstPosition.X; i <= lastPosition.X; i++)
+                    {
+                        board[i, firstPosition.Y] = 'S';//S represent as ship marker
+                        shipDictionary[i + "," + firstPosition.Y] = board[i, firstPosition.Y];
+                        if (string.IsNullOrEmpty(shipStartPosition))
+                            shipStartPosition = $"{i},{firstPosition.Y}";
+                    }
+                }
+                numberOfShips.Add(shipStartPosition, shipDictionary);
+            }
+
+            return board;
+        }
+
+
+        /// <summary>
+        /// Create board of 10 x 10 
+        /// </summary>
+        /// <returns></returns>
+        private static char[,] CreateBoard()
+        {
+            var board = new char[10, 10];
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = '.';
+                }
+            }
+
+            return board;
+        }
+
+        /// <summary>
+        /// Get Positions
+        /// </summary>
+        /// <param name="xyArray"></param>
+        /// <returns></returns>
+        private static Position GetPositions(string[] xyArray)
+        {
+            Position p = new Position
+            {
+                X = Convert.ToInt32(xyArray[0]),
+                Y = Convert.ToInt32(xyArray[1])
+            };
+            return p;
+        }
+    }
+
+    /// <summary>
+    /// Helper for Positioning
+    /// the ships
+    /// </summary>
+    class Position
+    {
+        public int X { get; set; } = -1;
+        public int Y { get; set; } = -1;
     }
 }
